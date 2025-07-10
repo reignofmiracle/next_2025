66dcb63c1b09e80cf5a8ad415ad4557852c5ee8d
reign <reignofmiracle@gmail.com>
2025-07-10 23:28:11 +0900

    update

---
+++ nextjs/react-foundations/ch09_installing_nextjs/app/page.js
@@ -0,0 +1,24 @@
+import LikeButton from "./like-button";
+
+function Header({ title }) {
+  console.log(title);
+  return <h1>{title ? title : "Default title"}</h1>;
+}
+
+export default function HomePage() {
+  const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
+
+  return (
+    <div>
+      <Header />
+      <Header title="React" />
+      <Header title="A new title" />
+      <ul>
+        {names.map((name) => (
+          <li>{name}</li>
+        ))}
+      </ul>
+      <LikeButton />
+    </div>
+  );
+}
